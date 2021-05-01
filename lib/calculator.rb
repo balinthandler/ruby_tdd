ca352e203c049dda756d567363986038bcb9ab54
Balint Handler <handlerbalint@gmail.com>
2021-05-01 12:02:36 +0200

    #multiply method tests

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,9 @@ class Calculator
   def add(*args)
     args.sum
   end
+  def multiply(*args)
+    result = 1
+    args.each {|num| result *= num}
+    result
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,4 +12,15 @@ describe Calculator do
       expect(calculator.add(3,7,5)).to eql(15)
     end
   end
+
+  describe "#multiply" do
+    it "returns the multiple of 2 numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(3,3)).to eql(9)
+    end
+    it "returns the multiple of more than 2 numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(2,4,4,3)).to eql(96)
+    end
+  end
 end
